0bc5bded220298679fc76fe2f83b68f646b7e1ed
1Pampu <piampianimartin@gmail.com>
2023-09-08 20:40:11 -0300

    translates

--- Project_1/practica_semaforo.ino
+++ Project_1/practica_semaforo.ino
@@ -1,33 +1,34 @@
-// La secuencia de encendido es rojo, naranja y verde
-// • Los tiempos de estado HIGH debe ser 3 s, 1 s, 1,5 s, respectivamente.
-// • Los tiempos de espera deben ser de 100 ms
-// • La secuencia debe reproducirse de forma infinita.
+// The ignition sequence is red, orange, and green.
+// • The HIGH state times should be 3 s, 1 s, 1.5 s, respectively.
+// • The waiting times should be 100 ms.
+// • The sequence should be played infinitely.
 
+// Define Pins
 const int redLED = 13;
 const int orangeLED = 12;
 const int greenLED = 11;
 
 void setup() {
-  // Declaracion pines
+  // LEDs SETUP
   pinMode(redLED,OUTPUT);
   pinMode(orangeLED,OUTPUT);
   pinMode(greenLED,OUTPUT);
 }
 
 void loop() {
-  // ROJO
+  // RED
   digitalWrite(redLED,HIGH);
   delay(3000);
   digitalWrite(redLED, LOW);
   // DELAY
   delay(100);
-  // NARANJA
+  // ORANGE
   digitalWrite(orangeLED, HIGH);
   delay(1000);
   digitalWrite(orangeLED, LOW);
   // DELAY
   delay(100);
-  // VERDE
+  // GREEN
   digitalWrite(greenLED, HIGH);
   delay(1500);
   digitalWrite(greenLED, LOW);
